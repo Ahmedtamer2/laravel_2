bfdcb858bfb33b02479c92936e12f6932d07e31d
Ahmed <tamerahmed005@gmail.com>
2025-06-27 00:56:45 +0300

    laravel_2

---
+++ resources/views/front/partials/footer.blade.php
@@ -0,0 +1,32 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style="margin-top: 6rem;">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-md-6 col-lg-4">
+                <h5 class="text-white mb-4">Get In Touch</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i></p>
+                <p><i class="fa fa-phone-alt me-3"></i></p>
+                <p><i class="fa fa-envelope me-3"></i></p>
+             
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h5 class="text-white mb-4">Quick Link</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Privacy Policy</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">Career</a>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h5 class="text-white mb-4">Popular Link</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Privacy Policy</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">Career</a>
+            </div>
+        </div>
+    </div>
+    @include('front.partials.copyright')
+</div>
+<!-- Footer End -->
